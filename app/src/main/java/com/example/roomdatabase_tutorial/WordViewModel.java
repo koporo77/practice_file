bbd5066d2dd77ddd3fb77db8c8144ff777ef6035
koporo77 <guswns2847@gmail.com>
2020-08-06 12:03:44 +0900

    roomdatabase_example

---
+++ app/src/main/java/com/example/roomdatabase_tutorial/WordViewModel.java
@@ -0,0 +1,35 @@
+package com.example.roomdatabase_tutorial;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
+
+//Don't keep a reference to a context that has a shorter lifecycle than your ViewModel! Examples are:
+//
+//Activity
+//Fragment
+//View
